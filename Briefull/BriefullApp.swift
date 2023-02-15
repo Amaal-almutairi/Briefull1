4227f3caac0c2076d35687e023e3b365603538c4
amaal <amaal772@gmail.com>
2023-02-15 19:27:06 +0300

    Initial Commit

---
+++ Briefull/BriefullApp.swift
@@ -0,0 +1,17 @@
+//
+//  BriefullApp.swift
+//  Briefull
+//
+//  Created by Amaal Almutairi on 24/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct BriefullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
